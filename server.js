0c6b1197aff8081de5dd71d77e6615c6d669fb1d
Kiran Bhatt <kb_43@outlook.com>
2018-10-17 11:54:05 +0545

    database query with API response created

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'data-sfs'
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'ajax-live-search'
 });
 
 connection.connect();
@@ -15,8 +16,23 @@ app.set('views', __dirname+ '/views')
 app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.render('index.html')
+})
+
+app.get('/search', (req, res) => {
+  name = req.query.key;
+  connection.query('SELECT * from tbl_customer where CustomerName LIKE "%'+name+'%"', (err, result, field) => {
+    if(err) throw err;
+    var data = [];
+    for(i=0; i< result.length; i++) {
+      data.push(result[i].CustomerName)
+    }
+    res.end(JSON.stringify(data))
+  })
 })
 
 app.listen(3000, (err) => {
